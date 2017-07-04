d3906418e10042cca5259386ac5e00975a9415a8
bhuvaneshwaran <bhuvaneshwaran1994@gmail.com>
2017-07-04 19:53:17 +0530

    first commit

---
+++ src/pages/home/home.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import {NewPage} from '../new/new';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+onLoadNewPage()
+{
+  console.log("onLoadNewPage");
+  this.navCtrl.push(NewPage)
+}
+}
